94e5f0ac6198898a48a75118273149e6e9a16087
Sayooj Samuel <31436104+sayoojsamuel@users.noreply.github.com>
2026-06-10 00:10:44 +0530

    Add: validate-parentheses - submission-2

---
+++ Data Structures & Algorithms/validate-parentheses/submission-2.cpp
@@ -0,0 +1,27 @@
+class Solution {
+   public:
+    bool isValid(string s) {
+        if (s.length() % 2 != 0) return false;
+        
+        vector<char> stack;
+        unordered_map<char, char> bracket_pairs = {{'(', ')'}, {'{', '}'}, {'[', ']'}};
+        for (char c : s) {
+            cout << " c: " << c << endl;
+            if (string("({[").find(c) != string::npos) {
+                stack.push_back(c);
+            } else {
+                if (stack.empty()) return false;
+                char top = stack.back();
+                cout << " top: " << top << endl;
+                if (c == bracket_pairs[top])
+                    stack.pop_back();
+                else
+                    return false;
+            }
+        }
+        if (!stack.empty())
+            return false;
+        else
+            return true;
+    }
+};
